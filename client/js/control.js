5e1a32a07d7791235cfc62e2c0c7f86523cef3b8
bhy2001 <88808104+bhy2001@users.noreply.github.com>
2022-12-07 00:28:14 -0600

    start control.js, a bit done, a lot remains undone. mal lam tiep nha dm

--- client/js/control.js
+++ client/js/control.js
@@ -6,5 +6,19 @@
 const SERVER_URL = "";
 
 async function SignUp() {
-  var username = 
+  let username = document.getElementById("username").value;
+  let studentid = document.getElementById("student-id").value;
+  let email = document.getElementById("email").value;
+  let phonenumber = document.getElementById("phone-number").value;
+  let password = document.getElementById("password").value;
+  let confirmPassword = document.getElementById("password-cf").value;
+  console.log(
+    SERVER_URL +
+      `/api/signup?username=${username}&student_id=${studentid}&email=${email}&phone_number=${phonenumber}&password=${password}&confirmPassword=${confirmPassword}`
+  );
+  //   let request = await fetch(SERVER_URL + `/api/signup/?username=${username}&student_id=${studentid}&email=${email}&phone_number=${phonenumber}&password=${password}&confirmPassword=${confirmPassword}`)
+  //     .then((response) => response.json())
+  //     .then((response) => {
+
+  //     })
 }
